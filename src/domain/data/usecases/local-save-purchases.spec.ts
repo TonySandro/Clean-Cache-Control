d414315d35366c880706033b923398ef7be31cda
devTiconnecte <85088970+TonySandro@users.noreply.github.com>
2022-01-13 16:43:05 -0300

    ensure delete cache with correct key

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -2,19 +2,21 @@ class LocalSavePurchases {
     constructor(private readonly cacheStore: CacheStore) { }
 
     async save(): Promise<void> {
-        this.cacheStore.delete()
+        this.cacheStore.delete('purchases')
     }
 }
 
 interface CacheStore {
-    delete: () => void
+    delete: (key: string) => void
 }
 
 class CacheStoreSpy implements CacheStore {
     deleteCallsCount = 0
+    key: string
 
-    delete (): void {
+    delete(key: string): void {
         this.deleteCallsCount++
+        this.key = key
     }
 }
 
@@ -26,7 +28,7 @@ type SutTypes = {
 const makeSut = (): SutTypes => {
     const cacheStore = new CacheStoreSpy()
     const sut = new LocalSavePurchases(cacheStore)
-    return { 
+    return {
         sut,
         cacheStore
     }
@@ -34,15 +36,22 @@ const makeSut = (): SutTypes => {
 //Garante que nao tenha error por modificacoes no construtor
 describe('LocalSavePurchases', () => {
     test('Should not delete cache on sut.init', () => {
-        const {cacheStore} = makeSut()
+        const { cacheStore } = makeSut()
         new LocalSavePurchases(cacheStore)
         expect(cacheStore.deleteCallsCount).toBe(0)
     })
 
     //Garantir que antes de salvar o cache, ele limpe o cache atual
-    test('Should  delete old cache on sut.save', async () => {
-        const {cacheStore, sut} = makeSut()
+    test('Should delete old cache on sut.save', async () => {
+        const { cacheStore, sut } = makeSut()
         await sut.save()
         expect(cacheStore.deleteCallsCount).toBe(1)
+    })
+
+    //Precisa da chave correta para deletar o cache
+    test('Should call delete with correct key', async () => {
+        const { cacheStore, sut } = makeSut()
+        await sut.save()
+        expect(cacheStore.key).toBe('purchases')
     })
 })
