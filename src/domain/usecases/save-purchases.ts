225de42eca5ecb3ff7e4c8d085ae38234fa7bf89
devTiconnecte <85088970+TonySandro@users.noreply.github.com>
2022-01-13 16:00:08 -0300

    add SavePurchases use case

---
+++ src/domain/usecases/save-purchases.ts
@@ -0,0 +1,13 @@
+export interface SavePurchases {
+    save: (purchases: Array<SavePurchases.params>)=> Promise<void>
+
+}
+
+namespace SavePurchases {
+    export type params ={
+        id: string
+        date: Date
+        value: number
+    }
+
+}
