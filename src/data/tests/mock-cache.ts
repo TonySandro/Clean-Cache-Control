5cefee064516abcb996795f70723a85bbafacc5d
devTiconnecte <85088970+TonySandro@users.noreply.github.com>
2022-01-13 22:34:52 -0300

    move CacheStoreSpy to its own file

---
+++ src/data/tests/mock-cache.ts
@@ -0,0 +1,31 @@
+import { CacheStore } from "@/data/protocols/cache"
+import { SavePurchases } from "@/domain/usecases"
+
+export class CacheStoreSpy implements CacheStore {
+    deleteCallsCount = 0
+    insertCallsCount = 0
+    deleteKey: string
+    insertKey: string
+    insertValues: Array<SavePurchases.params> = []
+
+    delete(key: string): void {
+        this.deleteCallsCount++
+        this.deleteKey = key
+    }
+    insert(key: string, value: any): void {
+        this.insertCallsCount++
+        this.deleteKey = key
+        this.insertKey = key
+        this.insertValues = value
+
+    }
+
+    simulateDeleteError(): void {
+        jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => { throw new Error() })
+
+    }
+    simulateInsertError(): void {
+        jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => { throw new Error() })
+
+    }
+}
--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -1,36 +1,6 @@
-import { CacheStore } from '@/data/protocols/cache'
-import { SavePurchases } from '@/domain/usecases'
 import { LocalSavePurchases } from './local-save-purchases'
 import { mockPurchase } from '../../tests/mock-puchases'
-
-class CacheStoreSpy implements CacheStore {
-    deleteCallsCount = 0
-    insertCallsCount = 0
-    deleteKey: string
-    insertKey: string
-    insertValues: Array<SavePurchases.params> = []
-
-    delete(key: string): void {
-        this.deleteCallsCount++
-        this.deleteKey = key
-    }
-    insert(key: string, value: any): void {
-        this.insertCallsCount++
-        this.deleteKey = key
-        this.insertKey = key
-        this.insertValues = value
-
-    }
-
-    simulateDeleteError (): void {
-        jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => { throw new Error() })
-
-    }
-    simulateInsertError (): void {
-        jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => { throw new Error() })
-
-    }
-}
+import { CacheStoreSpy } from '../../tests/mock-cache'
 
 
 type SutTypes = {
